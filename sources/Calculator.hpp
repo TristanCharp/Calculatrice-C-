6f9fcb7ef88f586ada66b186a4ec020e5ceb8731
monnier <monnier.jacques.lgm@gmail.com>
2019-03-28 15:04:56 +0100

    Calculator.hpp
    Ce que j'ai rajouté dans Calculator.hpp tel qu'un attribu operandes de la classe Pile et ses methodes flush et append

---
+++ sources/Calculator.hpp
@@ -0,0 +1,11 @@
+class Calculator{
+    private:
+        Pile operandes;
+    public:
+////////JACQUES//////////////
+		void appendPile(string calcul);
+		void flushPile(void);
+		
+/////////////////////////////
+
+}
